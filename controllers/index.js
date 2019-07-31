38657c8824ea4924b0bb21d1823c511e4fe260ef
manoj <manoj@inflolabs.com>
2019-07-31 16:45:04 -0400

    changes

--- controllers/index.js
+++ controllers/index.js
@@ -122,7 +122,7 @@ exports.sendConnection = function (req, res) {
     var query = [
         'Match (a:User{email: {sid}})',
         'Match (b:User{email: {rid}})',
-        'Create (a)-[r:Has_Connection{staus: false, uuid: {id}}]-(b)',
+        'Create (a)-[r:Has_Connection{status: false, uuid: {id}}]->(b)',
         'Return a,b,r'
     ].join('\n');
 
@@ -139,6 +139,65 @@ exports.sendConnection = function (req, res) {
         .subscribe({
             onNext: function (records) {
                 data.push(records.toObject());
+            },
+            onCompleted: function () {
+                session.close();
+                res.send(data);
+            },
+            onError: function (error) {
+                console.log(error);
+                res.send(error);
+            }
+        })
+}
+
+exports.getConnections = function (req, res) {
+    var query = [
+        'Match (a:User{email: {sid}})-[r:Has_Connection{status: false}]-(b)',
+        'Return b'
+    ].join('\n');
+
+    var params = {
+        sid: req.query.sid
+    }
+
+    var data = [];
+
+    session
+        .run(query, params)
+        .subscribe({
+            onNext: function (records) {
+                data.push(records.toObject().b.properties);
+            },
+            onCompleted: function () {
+                session.close();
+                res.send(data);
+            },
+            onError: function (error) {
+                console.log(error);
+                res.send(error);
+            }
+        })
+}
+
+
+exports.myConnections = function (req, res) {
+    var query = [
+        'Match (a:User{email: {sid}})-[r:Has_Connection{status: true}]-(b)',
+        'Return b'
+    ].join('\n');
+
+    var params = {
+        sid: req.query.sid
+    }
+
+    var data = [];
+
+    session
+        .run(query, params)
+        .subscribe({
+            onNext: function (records) {
+                data.push(records.toObject().b.properties);
             },
             onCompleted: function () {
                 session.close();
@@ -257,7 +316,8 @@ exports.getposts = function (req, res) {
 
     var query = [
         'Match (u:User{email: {email}})-[r:Has_Post]->(p:Post)',
-        'Return u,p'
+        'Match (u:User{email: {email}})-[r1:Has_Connection{status: true}]-(b:User)-[r2:Has_Post]->(p1:Post)',
+        'Return u,p,p1,b'
     ].join('\n');
 
 
@@ -297,6 +357,113 @@ exports.getusers = function (req, res) {
         .subscribe({
             onNext: function (records) {
                 data.push(records.toObject().u.properties);
+            },
+            onCompleted: function () {
+                session.close();
+                res.send(data);
+            },
+            onError: function (error) {
+                console.log(error);
+                res.send(error);
+            }
+        })
+}
+
+exports.createChild = function (req, res) {
+    var query = [
+        'Match (p:User{email: {parentEmail}})',
+        'Create (u:User{email: {emailId}, uuid:{id}, username: {username}, dob: {dob}, child: true})',
+        'Create (p)-[r:Has_Child]->(u)',
+        'Return u'
+    ].join('\n');
+
+
+    var params = {
+        id: uuid(),
+        emailId: req.body.email,
+        parentEmail: req.body.pemail,
+        password: req.body.password,
+        username: req.body.firstname,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        phone: req.body.phone,
+        dob: req.body.dob
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(params.emailId, params.password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error);
+        // ...
+      });
+    var data = [];
+
+    session
+        .run(query, params)
+        .subscribe({
+            onNext: function (records) {
+                data.push(records.toObject());
+            },
+            onCompleted: function () {
+                session.close();
+                res.send(data);
+            },
+            onError: function (error) {
+                console.log(error);
+                res.send(error);
+            }
+        })
+}
+
+exports.getChildConnections = function (req, res) {
+
+    var query = [
+        'Match (u:User {email: {email}})-[r:Has_Child]->(c:User)-[q:Has_Connection{status: false}]-(b:User)',
+        'Return c,b'
+    ].join('\n');
+
+    var params = {
+        email: req.query.email
+    }
+
+    var data = [];
+
+    session
+        .run(query, params)
+        .subscribe({
+            onNext: function (records) {
+                data.push(records.toObject());
+            },
+            onCompleted: function () {
+                session.close();
+                res.send(data);
+            },
+            onError: function (error) {
+                console.log(error);
+                res.send(error);
+            }
+        })
+
+}
+
+exports.getChild = function (req, res) {
+    var query = [
+        'Match (u:User {email: {email}})-[r:Has_Child]->(c:User)',
+        'Return c'
+    ].join('\n')
+
+    var data = [];
+
+    var params = {
+        email: req.query.email
+    }
+
+    session
+        .run(query, params)
+        .subscribe({
+            onNext: function (records) {
+                data.push(records.toObject().c.properties);
             },
             onCompleted: function () {
                 session.close();
--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,19 @@ app.post('/api/createStatus', controller.createStatus);
 app.get('/api/getposts', controller.getposts);
 
 app.get('/api/users', controller.getusers);
+
+app.post('/api/createchild', controller.createChild);
+
+app.get('/api/getchildern', controller.getChild);
+
+app.get('/api/myconnections', controller.myConnections);
+
+app.post('/api/sendconnection', controller.sendConnection);
+
+app.get('/api/getConnections', controller.getConnections);
+
+app.post('/api/makeconnection', controller.makeConnection);
+
+app.post('/api/deleteConnection', controller.deleteConnection);
+
+app.get('/api/childConnects', controller.getChildConnections);
